ec2550266e5f2bbcfd0e1bc4b1a2fc44e600fc28
sg23x <soumyagupta2307@gmail.com>
2020-06-12 01:38:48 +0530

    added: dealer and player pages

--- lib/UI/landing.dart
+++ lib/UI/landing.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:poker/UI/dealerLanding.dart';
+import 'package:poker/UI/playerLanding.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text(
+                  'DEALER',
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => DealerLandingPage(),
+                    ),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text(
+                  'PLAYER',
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => PlayerLandingPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/UI/playerLanding.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PlayerLandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
